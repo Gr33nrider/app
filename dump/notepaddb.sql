788285b22c05f5e9f1594123117fb1e95354e745
Gr33nrider <142820715+Gr33nrider@users.noreply.github.com>
2025-12-16 19:01:39 +0300

    Update notepaddb.sql

--- dump/notepaddb.sql
+++ dump/notepaddb.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 -- База данных: `notepaddb`
 --
 
-CREATE DATABASE IF NOT EXISTS `notepaddb` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `notepaddb` CHARACTER SET cp1251 COLLATE cp1251_general_ci;
 USE notepaddb;
 
 -- --------------------------------------------------------
